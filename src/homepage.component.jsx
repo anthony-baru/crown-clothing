16f5b2f24b77f41439e62058a6418e0a48800665
anthony baru <anthony.baru@apollo.co.ke>
2022-06-16 11:51:00 +0300

    test pr

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -12,7 +12,7 @@ const Homepage = () => {
         <div className="menu-item">
           <div className="content">
             <h1 className="title">JACKETS</h1>
-            <span className="subtitle">SHOP NOW</span>
+            <span className="subtitle">SHOP NO</span>
           </div>
         </div>
         <div className="menu-item">
