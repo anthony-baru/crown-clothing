ba028311d7bf70eab4c7522254ba1c7a9528554d
anthony baru <anthony.baru@apollo.co.ke>
2022-06-21 13:28:27 +0300

    implemented sign up with email password

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
+import { toast } from "react-toastify";
+import "./sign-up.styles.scss";
+
+class SignUp extends Component {
+  state = { displayName: "", email: "", password: "", confirmPassword: "" };
+  render() {
+    const { displayName, email, password, confirmPassword } = this.state;
+    return (
+      <div className="sign-up">
+        <h2 className="title">I do not have an account</h2>
+        <span className="">Sign up with your email and password</span>
+        <form action="" className="sign-up-form" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="displayName"
+            value={displayName}
+            onChange={this.handleChange}
+            label="Display Name"
+            required
+          ></FormInput>
+          <FormInput
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+            label="Email"
+            required
+          ></FormInput>
+
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+            label="Password"
+            required
+          ></FormInput>
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={this.handleChange}
+            label="Confirm Password"
+            required
+          ></FormInput>
+          <CustomButton type="submit">Sign Up</CustomButton>
+        </form>
+      </div>
+    );
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { displayName, email, password, confirmPassword } = this.state;
+
+      if (password !== confirmPassword) {
+        toast.error("Passwords do not match.");
+        return;
+      }
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await createUserProfileDocument(user, { displayName });
+
+      this.setState({
+        displayName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
+      toast.success(`Registration successful. You can login.`);
+    } catch (e) {
+      toast.error(`Error occurred ${e.message}`);
+    }
+  };
+}
+
+export default SignUp;
