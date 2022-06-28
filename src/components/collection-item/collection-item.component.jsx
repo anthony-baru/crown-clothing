ff1858d0ca6a3f2ecd6c8d9c200924e86d85acb9
anthony baru <anthony.baru@apollo.co.ke>
2022-06-28 14:56:59 +0300

    changed props sent to collection item

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart.actions";
+import { toast } from "react-toastify";
 import "./collection-item.style.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    ></div>
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <div className="price">{price}</div>
+const CollectionItem = ({ item, addItem }) => {
+  // const { name, price, imageUrl } = item;
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${item.imageUrl})`,
+        }}
+      ></div>
+      <div className="collection-footer">
+        <span className="name">{item.name}</span>
+        <div className="price">{item.price}</div>
+      </div>
+      <CustomButton
+        className="custom-button"
+        size="sm"
+        onClick={() => {
+          toast.success(`${item.name} added to cart.`);
+          return addItem(item);
+        }}
+      >
+        Add to Cart
+      </CustomButton>
     </div>
-    <CustomButton className="custom-button" size="sm">
-      Add to Cart
-    </CustomButton>
-  </div>
-);
+  );
+};
 
-export default CollectionItem;
+const mapActionToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapActionToProps)(CollectionItem);
